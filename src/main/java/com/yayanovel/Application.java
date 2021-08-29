097febeb044e9d4f47bfda55e3aacce8e910ca40
hudashijiaolin@163.com <Shi@123456>
2021-08-29 22:59:52 +0800

    添加数据库操作的相关类

--- src/main/java/com/yayanovel/Application.java
+++ src/main/java/com/yayanovel/Application.java
@@ -1,9 +1,10 @@
-package com.yayanovle;
+package com.yayanovel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
